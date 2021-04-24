8587e7746b6e390ab87d6a0d2cfe35525ba575ed
steeltx <realsteel.gnu@gmail.com>
2021-04-23 23:25:31 -0500

    Se conecta con firebase

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,19 @@
   <router-view />
 </template>
 
+<script>
+import { onMounted } from "vue";
+import { auth } from "./utils/firebase";
+export default {
+  name: "App",
+  setup() {
+    onMounted( () => {
+      auth.onAuthStateChanged((user) => {
+        console.log(user)
+      });
+    }); 
+  },
+};
+</script>
+
 <style></style>
