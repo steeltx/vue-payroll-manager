49bae87da8d7e138efd7ece118caad3b0e5faa7f
steeltx <realsteel.gnu@gmail.com>
2021-06-05 18:21:12 -0500

    Validar formulario cambiar nombre

--- src/components/Account/ChangeName.vue
+++ src/components/Account/ChangeName.vue
@@ -1,13 +1,53 @@
 <template>
-  <form class="ui form change-name">
-    <input type="text" placeholder="Nombre y apellidos" />
+  <form class="ui form change-name" @submit.prevent="onChangeName">
+    <input
+      type="text"
+      placeholder="Nombre y apellidos"
+      v-model="name"
+      :class="{ error: formError }"
+    />
     <button type="submit" class="ui button primary">Actualizar</button>
   </form>
 </template>
 
 <script>
+import { ref } from "vue";
+import * as Yup from "yup";
 export default {
   name: "ChangeName",
+  setup() {
+    let name = ref("");
+    let formError = ref(false);
+
+    const schemaForm = Yup.object().shape({
+      name: Yup.string()
+        .min(6, true)
+        .required(true),
+    });
+
+    const onChangeName = async () => {
+      formError.value = false;
+
+      console.log("Cambiando nombre");
+      console.log("nuevo nombre: ", name.value);
+
+      try {
+        await schemaForm.validate({ name: name.value }, { abortEarly: false });
+        console.log("Cambio correcto");
+      } catch (err) {
+        console.log("Error");
+        err.inner.forEach((error) => {
+          formError.value = error.message;
+        });
+      }
+    };
+
+    return {
+      name,
+      onChangeName,
+      formError,
+    };
+  },
 };
 </script>
 
@@ -15,11 +55,11 @@ export default {
 .ui.form.change-name {
   text-align: center;
   input.error {
-      border-color: #faa;
-      background-color: #ffeded;
+    border-color: #faa;
+    background-color: #ffeded;
   }
-  .ui.button{
-      margin: 20px 0 0 0;
+  .ui.button {
+    margin: 20px 0 0 0;
   }
 }
 </style>
