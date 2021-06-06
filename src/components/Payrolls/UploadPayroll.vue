d41120a29fb5f52d2aabc0bb1c25f89d5ecf9c9c
steeltx <realsteel.gnu@gmail.com>
2021-06-06 00:09:08 -0500

    Formulario subir nominas

---
+++ src/components/Payrolls/UploadPayroll.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="upload-payroll">
+    <button class="ui button primary">
+      Nueva nomina
+    </button>
+    <form class="ui form upload-payroll__form">
+      <div class="field">
+        <label for="file" class="ui icon button">
+          <i class="file icon"></i>
+          Seleccionar nomina
+        </label>
+        <input type="file" id="file" style="display:none" />
+      </div>
+      <div class="field">
+        <div class="ui calendar">
+          <div class="ui input left icon">
+            <input type="date" />
+          </div>
+        </div>
+      </div>
+      <button class="ui button positive">
+        Subir nomina
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UploadPayroll",
+};
+</script>
+
+<style lang="scss" scoped>
+.upload-payroll {
+  &__form {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+}
+</style>
--- src/views/Payrolls.vue
+++ src/views/Payrolls.vue
@@ -1,22 +1,28 @@
 <template>
-<BasicLayout>
-  <h1>Estamos en Payrolls</h1>
-</BasicLayout>
+  <BasicLayout>
+    <div class="payrolls">
+      <h1>Mis nominas</h1>
+      <UploadPayroll />
+    </div>
+  </BasicLayout>
 </template>
 
 <script>
-
 import BasicLayout from "../layouts/BasicLayout";
-
+import UploadPayroll from "../components/Payrolls/UploadPayroll";
 
 export default {
-  name:"Payrolls",
-  components:{
-    BasicLayout
-  }
-}
+  name: "Payrolls",
+  components: {
+    BasicLayout,
+    UploadPayroll,
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.payrolls {
+  margin: 50px 0;
+  text-align: center;
+}
 </style>
