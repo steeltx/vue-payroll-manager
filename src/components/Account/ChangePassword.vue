612dae5912909203d8e673e3693bfd3214c6bdc3
steeltx <realsteel.gnu@gmail.com>
2021-06-05 22:52:28 -0500

    Validando formulario cambio contraseña

--- src/components/Account/ChangePassword.vue
+++ src/components/Account/ChangePassword.vue
@@ -1,14 +1,29 @@
 <template>
-  <form class="ui form change-password">
+  <form class="ui form change-password" @submit.prevent="onChangePassword">
     <div class="three fields">
       <div class="field">
-        <input type="password" placeholder="Contraseña actual" />
+        <input
+          type="password"
+          placeholder="Contraseña actual"
+          v-model="formData.password"
+          :class="{ error: formError.password }"
+        />
       </div>
       <div class="field">
-        <input type="password" placeholder="Nueva contraseña" />
+        <input
+          type="password"
+          placeholder="Nueva contraseña"
+          v-model="formData.newPassword"
+          :class="{ error: formError.newPassword }"
+        />
       </div>
       <div class="field">
-        <input type="password" placeholder="Repite contraseña" />
+        <input
+          type="password"
+          placeholder="Repite contraseña"
+          v-model="formData.newPasswordRepeat"
+          :class="{ error: formError.newPasswordRepeat }"
+        />
       </div>
     </div>
     <button type="submit" class="ui button primary">
@@ -18,8 +33,41 @@
 </template>
 
 <script>
+import { ref } from "vue";
+import * as Yup from "yup";
+
 export default {
   name: "ChangePassword",
+  setup() {
+    let formData = {};
+    let formError = ref({});
+
+    const schemaForm = Yup.object().shape({
+      password: Yup.string().required(true),
+      newPassword: Yup.string().required(true),
+      newPasswordRepeat: Yup.string()
+        .required(true)
+        .oneOf([Yup.ref("newPassword")], true),
+    });
+
+    const onChangePassword = async () => {
+      formError.value = {};
+      try {
+        await schemaForm.validate(formData, { abortEarly: false });
+        console.log("ok");
+      } catch (err) {
+        err.inner.forEach((error) => {
+          formError.value[error.path] = error.message;
+        });
+      }
+    };
+
+    return {
+      formData,
+      onChangePassword,
+      formError,
+    };
+  },
 };
 </script>
 
