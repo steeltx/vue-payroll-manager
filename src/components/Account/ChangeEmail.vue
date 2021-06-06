cac7f1ae34d59ac6de8ea9f4e83e93fa6554e331
steeltx <realsteel.gnu@gmail.com>
2021-06-05 20:50:52 -0500

    Validación formulario email

--- src/components/Account/ChangeEmail.vue
+++ src/components/Account/ChangeEmail.vue
@@ -1,37 +1,82 @@
 <template>
-  <form class="ui form change-email">
-
-      <div class="two fields">
-          <div class="field">
-              <input type="text" placeholder="Nuevo email">
-          </div>
-          <div class="field">
-              <input type="password" placeholder="Contraseña actual">
-          </div>
+  <form class="ui form change-email" @submit.prevent="onChangeEmail">
+    <div class="two fields">
+      <div class="field">
+        <input
+          type="text"
+          placeholder="Nuevo email"
+          v-model="formData.email"
+          :class="{ error: formError.email }"
+        />
       </div>
-      <button type="submit" class="ui button primary">
-          Actualizar
-      </button>
-
+      <div class="field">
+        <input
+          type="password"
+          placeholder="Contraseña actual"
+          v-model="formData.password"
+          :class="{ error: formError.password }"
+        />
+      </div>
+    </div>
+    <p v-if="messageError">{{ messageError }}</p>
+    <button type="submit" class="ui button primary" :class="{ loading }">
+      Actualizar
+    </button>
   </form>
 </template>
 
 <script>
+import { ref } from "vue";
+import * as Yup from "yup";
+
 export default {
-    title:"ChangeEmail"
-}
+  title: "ChangeEmail",
+  setup() {
+    let formData = {};
+    let formError = ref({});
+    let messageError = ref("");
+    let loading = ref(false);
+
+    const schemaForm = Yup.object().shape({
+      email: Yup.string()
+        .email(true)
+        .required(true),
+      password: Yup.string().required(true),
+    });
+
+    const onChangeEmail = async () => {
+      formError.value = {};
+      loading.value = true;
+      try {
+        await schemaForm.validate(formData, { abortEarly: false });
+      } catch (err) {
+        err.inner.forEach((error) => {
+          formError.value[error.path] = error.message;
+        });
+      }
+      loading.value = false;
+    };
+
+    return {
+      formData,
+      formError,
+      messageError,
+      onChangeEmail,
+      loading,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    .ui.form.change-email{
-        text-align: center;
-        input.error{
-            border-color: #faa;
-            background-color: #ffeded;
-        }
-
-        .ui.button{
-            margin: 20px 0 0 0;
-        }
-    }
+.ui.form.change-email {
+  text-align: center;
+  input.error {
+    border-color: #faa;
+    background-color: #ffeded;
+  }
+  .ui.button {
+    margin: 20px 0 0 0;
+  }
+}
 </style>
