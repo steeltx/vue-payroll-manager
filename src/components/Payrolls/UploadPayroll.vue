1bef4d609555d9bf8ffc35bbb2935d0b37495a4c
steeltx <realsteel.gnu@gmail.com>
2021-06-06 00:17:33 -0500

    Animación de formulario payroll

--- src/components/Payrolls/UploadPayroll.vue
+++ src/components/Payrolls/UploadPayroll.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="upload-payroll">
-    <button class="ui button primary">
+    <button class="ui button primary" @click="showCloseForm">
       Nueva nomina
     </button>
-    <form class="ui form upload-payroll__form">
+    <form class="ui form upload-payroll__form" :class="{ open: showForm }">
       <div class="field">
         <label for="file" class="ui icon button">
           <i class="file icon"></i>
@@ -26,17 +26,40 @@
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "UploadPayroll",
+  setup() {
+    let showForm = ref(false);
+
+    const showCloseForm = () => {
+      showForm.value = !showForm.value;
+    };
+
+    return {
+      showForm,
+      showCloseForm,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .upload-payroll {
+  > .ui.button.primary {
+    margin-bottom: 30px;
+  }
   &__form {
     display: flex;
     align-items: center;
     flex-direction: column;
+    height: 0;
+    overflow: hidden;
+    transition: height 0.3s ease;
+
+    &.open {
+      height: 200px;
+    }
   }
 }
 </style>
